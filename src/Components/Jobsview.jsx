75b8fd7216b77af254ef0e096ba73d601940a03f
Kayode <drkay4sure@gmail.com>
2025-05-25 19:36:46 +0100

    great

--- src/Components/Jobsview.jsx
+++ src/Components/Jobsview.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Jobsview = () => {
   return (
     <section className="m-auto max-w-lg my-10 px-6">
-        <a href="jobs.html" className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All Jobs</a>
+        <a href="/jobs" className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All Jobs</a>
       </section>
   )
 }
